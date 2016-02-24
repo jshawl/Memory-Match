976255b4dd4d7625ecfadff4638442df2abde231
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-02-24 13:40:27 -0500

    game is done. let's add more

--- memory.js
+++ memory.js
@@ -1,27 +1,28 @@
 //will add the images afterward
-var imgCards=[getImage("avatars/leafers-seed"),
-    getImage("avatars/leafers-seedling"),
-    getImage("avatars/leafers-sapling"),
-    getImage("avatars/leafers-tree"),
-    getImage("avatars/leafers-ultimate"),
-    getImage("avatars/marcimus"),
-    getImage("avatars/mr-pants"),
-    getImage("avatars/mr-pink"),
-    getImage("avatars/old-spice-man"),
-    getImage("avatars/robot_female_1")
-    getImage("avatars/leafers-seed"),
-        getImage("avatars/leafers-seedling"),
-        getImage("avatars/leafers-sapling"),
-        getImage("avatars/leafers-tree"),
-        getImage("avatars/leafers-ultimate"),
-        getImage("avatars/marcimus"),
-        getImage("avatars/mr-pants"),
-        getImage("avatars/mr-pink"),
-        getImage("avatars/old-spice-man"),
-        getImage("avatars/robot_female_1")
+var imgCards=[ "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=w300",
+    "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=w300",
+    "https://g.twimg.com/Twitter_logo_blue.png",
+    "https://g.twimg.com/Twitter_logo_blue.png",
+    "http://us.mullenlowe.com/wp-content/uploads//2013/10/instagramlogo.jpg",
+    "http://us.mullenlowe.com/wp-content/uploads//2013/10/instagramlogo.jpg",
+    "http://theappshowbox.com/wp-content/uploads/2015/10/LinkedIn-Logo.png",
+    "http://theappshowbox.com/wp-content/uploads/2015/10/LinkedIn-Logo.png",
+    "https://62e528761d0685343e1c-f3d1b99a743ffa4142d9d7f1978d9686.ssl.cf2.rackcdn.com/files/93616/area14mp/image-20150902-6700-t2axrz.jpg",
+    "https://62e528761d0685343e1c-f3d1b99a743ffa4142d9d7f1978d9686.ssl.cf2.rackcdn.com/files/93616/area14mp/image-20150902-6700-t2axrz.jpg",
+    "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+    "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+    "http://archiveteam.org/images/1/15/Apple-logo.jpg",
+    "http://archiveteam.org/images/1/15/Apple-logo.jpg",
+    "http://dri1.img.digitalrivercontent.net/Storefront/Site/msusa/images/promo/en-US/msstore_400x400.jpg",
+    "http://dri1.img.digitalrivercontent.net/Storefront/Site/msusa/images/promo/en-US/msstore_400x400.jpg",
+    "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-glossy-green-orbs-icons-alphanumeric/102895-3d-glossy-green-orb-icon-alphanumeric-dollar-sign.png",
+    "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-glossy-green-orbs-icons-alphanumeric/102895-3d-glossy-green-orb-icon-alphanumeric-dollar-sign.png",
+    "http://www.multimedialab.be/blog/wp-content/uploads/2009/04/logo_ps.gif",
+    "http://www.multimedialab.be/blog/wp-content/uploads/2009/04/logo_ps.gif"
   ];
 var flipCard=[];
 var flippedCards = 0;
+var counter =0
 
 function shuffleCards(){
   Array.prototype.shuffle = function()
@@ -43,27 +44,35 @@ function makeBoard(){
   var board = '';
   for( var i = 0; i<imgCards.length; i++){
     board += '<div class = "card">'
-    board += '<img src = "'+ imgCards[i] +'"/>'
+    board += '<img src = "'+imgCards[i]+'"/>'
     board += '</div>'
   }
-document.getElementById('gamePlace').innerHTML = board;
-$("img").hide();
+  document.getElementById('gamePlace').innerHTML = board;
+  document.getElementsByTagName("p")[0].innerHTML = "flipped cards: "+ counter;
+  $("img").hide();
 }
+makeBoard();
 
 $(".card").click(function(){
-  if (flipCard.length < 2 &&  ($(this).children("img").hasClass("up")) === false) {
+  if ((flipCard.length < 2) && ($(this).children("img").hasClass("up")) === false) {
+    $(this).children("img").css("background-color","white");
     $(this).children("img").show();
     $(this).children("img").addClass("up");
-    if (flipCard.length = 0){
+    if (flipCard.length === 0){
       flipCard.push($(this).children("img").attr("src"));
-      if (flipCard.length = 1) {
+      counter++;
+      document.getElementsByTagName("p")[0].innerHTML = "flipped cards: "+ counter;
+    }
+    else if (flipCard.length === 1) {
         flipCard.push($(this).children("img").attr("src"));
+        counter++;
+        document.getElementsByTagName("p")[0].innerHTML = "flipped cards: "+ counter;
         if(flipCard[0]===flipCard[1]){
-          $(".card").children("img[src='" + flipCard[1] + "']").addClass("match");
+          $(".card").children("img[src='"+flipCard[1]+"']").addClass("match");
           flippedCards += 2;
           flipCard =[];
           if(flippedCards === imgCards.length){
-            alert("You Win");
+            document.getElementsByTagName("p")[0].innerHTML = "You Completed the Game. Your flip count is "+ counter;
           }
         }
         else{
@@ -73,11 +82,9 @@ $(".card").click(function(){
           }, 500);
           flipCard =[];
         }
+     }
+   }
 
-
-      }
-    }
-  }
 });
 
 
@@ -86,4 +93,5 @@ $(".card").click(function(){
 
 
 
-// window.addEventListener(makeBoard());
+
+// // window.addEventListener(makeBoard());
