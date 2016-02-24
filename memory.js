3090d66bff482706ffe3daff5a6f737a5b153e9b
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-02-24 07:13:21 -0500

    jquery is more powerful than I thought

--- memory.js
+++ memory.js
@@ -1,67 +1,89 @@
-var imgCards=[];
+//will add the images afterward
+var imgCards=[getImage("avatars/leafers-seed"),
+    getImage("avatars/leafers-seedling"),
+    getImage("avatars/leafers-sapling"),
+    getImage("avatars/leafers-tree"),
+    getImage("avatars/leafers-ultimate"),
+    getImage("avatars/marcimus"),
+    getImage("avatars/mr-pants"),
+    getImage("avatars/mr-pink"),
+    getImage("avatars/old-spice-man"),
+    getImage("avatars/robot_female_1")
+    getImage("avatars/leafers-seed"),
+        getImage("avatars/leafers-seedling"),
+        getImage("avatars/leafers-sapling"),
+        getImage("avatars/leafers-tree"),
+        getImage("avatars/leafers-ultimate"),
+        getImage("avatars/marcimus"),
+        getImage("avatars/mr-pants"),
+        getImage("avatars/mr-pink"),
+        getImage("avatars/old-spice-man"),
+        getImage("avatars/robot_female_1")
+  ];
 var flipCard=[];
-var placedCard=[];
 var flippedCards = 0;
 
-function shuffleCards(mix){
-  img.sort(mix);
-}
-
-function mixUp(){
-  var randomValue = Math.random();
-  if(randomValue > 0.5){
-    return 1;
-  }else{
-    return -1;
-  }
+function shuffleCards(){
+  Array.prototype.shuffle = function()
+  {
+    var i = this.length, j, temp;
+    while ( --i )
+    {
+      j = Math.floor( Math.random() * (i + 1));
+      temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+    }
+  };
+  imgCards.shuffle();
 }
 
 function makeBoard(){
-  shuffleCards(mixUp());
-  for( var i; i<imgCards.length; i++){
-    var board ='<div id= "card" ' + i + '><div>';
+  shuffleCards();
+  var board = '';
+  for( var i = 0; i<imgCards.length; i++){
+    board += '<div class = "card">'
+    board += '<img src = "'+ imgCards[i] +'"/>'
+    board += '</div>'
   }
-  document.getElementById("gamePlace").innerHTML = board
-  checkIfCardIsMatch();
+document.getElementById('gamePlace').innerHTML = board;
+$("img").hide();
 }
-function checkIfCardIsMatch(){
-  document.getElementById("card");
-  for (var i=0 ; i <imgCards.length; i++){
-  card[i].addEventListener( "click", function(){
-    if(flipCard.length < 2){
-      this.innerHTML = imgCards[i];
-      if(flipCard.length==0){
-        flipCard.push(imgCards[i]);
-        placedCard.push(this.id);
-      else if (flipCard.length==1) {
-        flipCard.push(imgCards[i]);
-        placedCard.push(this.id);
+
+$(".card").click(function(){
+  if (flipCard.length < 2 &&  ($(this).children("img").hasClass("up")) === false) {
+    $(this).children("img").show();
+    $(this).children("img").addClass("up");
+    if (flipCard.length = 0){
+      flipCard.push($(this).children("img").attr("src"));
+      if (flipCard.length = 1) {
+        flipCard.push($(this).children("img").attr("src"));
         if(flipCard[0]===flipCard[1]){
+          $(".card").children("img[src='" + flipCard[1] + "']").addClass("match");
           flippedCards += 2;
           flipCard =[];
-          placedCard =[];
-          if(flippedCards = imgCards.length){
-            alert("You completed the game! Good job!")
+          if(flippedCards === imgCards.length){
+            alert("You Win");
           }
         }
-      }
+        else{
+          setTimeout(function() {
+          $("img").not(".match").hide();
+          $("img").not(".match").removeClass("up");
+          }, 500);
+          flipCard =[];
+        }
+
 
       }
-    else {
-        
     }
-    }
-  });
   }
-}
+});
 
 
 
 
-}
 
-function filpCardsOver(){
 
-}
 
-window.addEventListener(makeBoard());
+// window.addEventListener(makeBoard());
